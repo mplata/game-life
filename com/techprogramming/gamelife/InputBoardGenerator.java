285aa2589012461c6994f86d3ea40bba7a16949f
Misael <ingmisaelzazueta@gmail.com>
2019-09-02 09:58:52 -0600

    Validaciones

--- com/techprogramming/gamelife/InputBoardGenerator.java
+++ com/techprogramming/gamelife/InputBoardGenerator.java
@@ -5,22 +5,72 @@ import java.util.Scanner;
 public class InputBoardGenerator {
 	
 	public Board generateBoard() {
-		
+		int k = 0 , x = 0, y = 0, totalRows = 0, totalColumns = 0, organismosIniciales;
 		Scanner scn = new Scanner(System.in);
+		do {
 		System.out.println("Ingrese numero de filas");
-		int totalRows = scn.nextInt();
+		totalRows = scn.nextInt();
+		if (totalRows <= 1)
+		{
+			System.out.println("Introduzca un valor mayor a 1.");
+			k = 0;
+		}
+		else {
+			k = 1;
+		}
+		}while (k == 0);
+		do {
 		System.out.println("Ingrese numero de columnas");
-		int totalColumns = scn.nextInt();
-		
-		System.out.println("Ingrese numero de organismos iniciales");
-		int organismosIniciales = scn.nextInt();
+		totalColumns = scn.nextInt();
+		if (totalColumns <= 1) {
+			System.out.println("Introduzca un valor mayor a 1.");
+			k = 0;
+		}
+		else {
+			k = 1;
+		}
+		}while (k == 0);
+		do {
+			System.out.println("Ingrese numero de organismos iniciales");
+			organismosIniciales = scn.nextInt();
+			if(organismosIniciales < 1 ) {
+				System.out.println("Introduzca un valor mayor a 0.");
+				k = 0;
+			}
+			else {
+				k=1;
+			}
+		}while(k == 0);
 		Board board = new Board(totalRows, totalColumns);
-		
+
 		for(int i=0;i<organismosIniciales; i++) {
-			System.out.println("Ingrese X del organismo "+(i+1));
-			int x = scn.nextInt();
+			do
+			{
+				System.out.println("Ingrese X del organismo "+(i+1));
+				x = scn.nextInt();
+				if (x > totalRows || x < 0) {
+					System.out.println("!Error¡ Introduzca un numero valido.");
+					k = 0;
+				}
+				else {
+					k=1;
+					x=x-1;
+				}
+			}
+			
+				while (k == 0);
+			do {
 			System.out.println("Ingrese Y del organismo "+(i+1));
-			int y = scn.nextInt();
+			y = scn.nextInt();
+			if( y > totalColumns || y < 0 ) {
+				System.out.println("!Error¡ Introduzca un numero valido.");
+				k = 0;
+			}
+			else {
+				k = 1;
+				y = y-1;
+			}
+			}while (k == 0);
 			board.addOrganism(x, y);
 		}
 		//scn.close();
